65e462c6df20e3c190e2ddf0ac2eb341f601aae3
Joe McCann <joe.mccann@mailbox.org>
2026-07-11 22:03:25 -0400

    Add db flush script.

---
+++ flushdb.py
@@ -0,0 +1,11 @@
+import redis
+
+redis_client = redis.StrictRedis(
+    host='0.0.0.0', port=6379, db=0, decode_responses=True
+)
+
+print("flushing test db data")
+
+redis_client.flushdb()
+
+print("done")
